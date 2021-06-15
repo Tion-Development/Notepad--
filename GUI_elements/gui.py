049b65810984e9f0bbf08a7f9b5a35f117eb3aee
10882 <62587713+10882@users.noreply.github.com>
2021-06-15 14:23:10 +0300

    Added 'new' button

--- GUI_elements/gui.py
+++ GUI_elements/gui.py
@@ -61,18 +61,29 @@ def startgui():
                 pos = (150, 680)
             ))
 
+            wf.add_widget(Button(
+                text = 'new',
+                on_press = self.new,
+                size_hint = (0.1, .04),
+                pos = (300, 680)
+            ))
+
             self.root.add_widget(wf)
 
             
         def open(self, instance):
-            self.root.children[0].children[2].text = ''
+            self.root.children[0].children[3].text = ''
             #После равно нужнен результат ф-ции (там где '')
 
         def save(self, instance):
-            text = self.root.children[0].children[2].text
+            text = self.root.children[0].children[3].text
             print(text)
             #В переменной text лежит твой текст
             
+        def new(self, instance):
+            self.root.children[0].children[3].text = ''
+            #Cюда новый
+
         def on_start(self):
             th = threading.Thread(target= self.make_work_filed)
             th.start()
