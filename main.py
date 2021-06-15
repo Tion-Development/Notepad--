155bbcf412fd75ca9a90ab26662f0c5400d82005
Andrei Kazarin <61632926+L1mPeX@users.noreply.github.com>
2021-06-15 23:45:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from config import *
 from GUI_elements.gui import *
-
+from File_Reg.coutWords import *
+from File_Reg.textInp import *
+from File_Reg.isFileChanged import *
+from File_Reg.saveTemp import *
+import os
 
 if __name__ == "__main__":
-    startgui()
+    startgui(textInp)
