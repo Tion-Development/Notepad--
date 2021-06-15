c9ce5b7d0058f80d53da3152ccd5f00ad70a2a55
Andrei Kazarin <61632926+L1mPeX@users.noreply.github.com>
2021-06-15 23:42:42 +0300

    Update coutWords.py

--- File_Reg/coutWords.py
+++ File_Reg/coutWords.py
@@ -1,19 +1,28 @@
-fp = open("text.py", encoding="UTF-8")
-line = fp.readline()
-lineN = " "
-cntWords = 0
-cntWordsG = 0
-cntStr = 0
-while len(line) != 0:
-    for i in line:
-        if i == ",":
-            cntWordsG = cntWordsG + 1
-        if i == " ":
-            cntWords = cntWords + 1
-    lineN = line[::-1]
+import os
+
+def countWords(nameFile):
+    source = os.getcwd()+'\\temp\\'+nameFile+'.txt'
+    fp = open(source, encoding="UTF-8")
     line = fp.readline()
-    cntStr = cntStr + 1
-fp.close()
+    lineN = " "
+    cntWords = 0
+    cntWordsG = 0
+    cntStr = 0
+    while len(line) != 0:
+        for i in line:
+            if i == ",":
+                cntWordsG = cntWordsG + 1
+            if i == " ":
+                cntWords = cntWords + 1
+        lineN = line[::-1]
+        line = fp.readline()
+        cntStr = cntStr + 1
+    fp.close()
+    string = f"The number of words with a comma: {cntWordsG} \nThe number of words without a comma: {cntWords+cntStr}"
+    return string
+
+# if __name__ == '__main__':
+#     print(countwords("temp//text.py"))
 
 # The number of words with a comma: cntWordsG
 # The number of words without a comma: cntWords+cntStr
