cfbbaf9e194f1524eb393d1daf854a34f9224e5c
Andrei Kazarin <61632926+L1mPeX@users.noreply.github.com>
2021-06-15 23:57:15 +0300

    Update saveTemp.py

--- File_Reg/saveTemp.py
+++ File_Reg/saveTemp.py
@@ -1,9 +1,6 @@
 import os
 os.path.normpath(os.getcwd() + os.sep + os.pardir)
-from GUI_elemens.gui import returnText
 
-def saveTemp():
-  source = os.getcwd()+'\\temp\\tempText.txt'
-  fp = open(sourse, encoding="UTF-8")
-  text = returnText
-  
+source = os.getcwd()+'\\temp\\tempText.txt'
+fp = open(sourse, encoding="UTF-8")
+text = returnText
